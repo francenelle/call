7447b592c96065dd6410b19b408a2ecbf3a89bbc
juniortachom <constantjunior30@gmail.com>
2022-02-23 22:41:53 +0100

    design navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,37 +1,67 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import 
-{
-    MdVideocam,
-    MdOutlineHome,
-    MdNotificationsNone,
-    MdOutlineChat,
-    MdPeopleOutline
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  MdVideocam,
+  MdOutlineHome,
+  MdNotificationsNone,
+  MdOutlineChat,
+  MdPeopleOutline,
 } from "react-icons/md";
 
-
 const Navigation = () => {
-    return (
-        <div className='navigation'>
-            <NavLink exact to="/" activeClassName='nav-active'>
-                <MdOutlineHome />
-            </NavLink>
-            <NavLink exact to="/callvideo" activeClassName='nav-active'>
-                <MdVideocam />
-            </NavLink>
-            <NavLink exact to="/notification" activeClassName='nav-active'>
-                <MdNotificationsNone />
-            </NavLink>
-            <NavLink exact to="/message" activeClassName='nav-active'>
-                <MdOutlineChat />
-            </NavLink>
-            <NavLink exact to="/contact" activeClassName='nav-active'>
-                <MdPeopleOutline />
-            </NavLink>
-           
-            
-        </div>
-    );
+  return (
+    <div className="navigation">
+      <img src="./img/logo192.png" alt="logo" className="navigation__logo" />
+      <ul className="navigation__links">
+        <li className="navigation__link">
+          <NavLink exact to="/" activeClassName="nav-active">
+            <MdOutlineHome />
+          </NavLink>
+        </li>
+        <li className="navigation__link">
+          <NavLink
+            exact
+            to="/callvideo"
+            activeClassName="navigation__link--active"
+          >
+            <MdVideocam />
+          </NavLink>
+        </li>
+        <li className="navigation__link">
+          <NavLink
+            exact
+            to="/notification"
+            activeClassName="navigation__link--active"
+          >
+            <MdNotificationsNone />
+          </NavLink>
+        </li>
+        <li className="navigation__link">
+          <NavLink
+            exact
+            to="/message"
+            activeClassName="navigation__link--active"
+          >
+            <MdOutlineChat />
+          </NavLink>
+        </li>
+        <li className="navigation__link">
+          <NavLink
+            exact
+            to="/contact"
+            activeClassName="navigation__link--active"
+          >
+            <MdPeopleOutline />
+          </NavLink>
+        </li>
+      </ul>
+      <img
+        src="./img/profile.png"
+        alt="profil"
+        className="navigation__profil"
+      />
+    </div>
+  );
 };
 
 export default Navigation;
