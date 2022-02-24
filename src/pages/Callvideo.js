845d90d7df6807afadf0447896142e73a9a4eda8
juniortachom <constantjunior30@gmail.com>
2022-02-24 02:37:34 +0100

    design callvideo

--- src/pages/Callvideo.js
+++ src/pages/Callvideo.js
@@ -8,8 +8,16 @@ import {
   MdArrowBackIosNew,
   MdAllInbox,
   MdOutlineBrokenImage,
+  MdMicOff,
+  MdOutlineVideocamOff,
+  MdOutlineFullscreen,
+  MdVolumeUp,
 } from "react-icons/md";
 import { FiSend } from "react-icons/fi";
+import { RiRecordCircleFill } from "react-icons/ri";
+import { BsPlusSquareFill, BsSoundwave } from "react-icons/bs";
+import { VscScreenFull } from "react-icons/vsc";
+import { IoLogOut } from "react-icons/io5";
 
 const Callvideo = () => {
   const [me, setMe] = useState("");
@@ -51,6 +59,113 @@ const Callvideo = () => {
               class X
             </Link>
           </header>
+          <main className="main">
+            <div className="cta">
+              <p className="record">
+                <RiRecordCircleFill /> Record 00:25:14
+              </p>
+              <p className="invitepeople">
+                <BsPlusSquareFill /> Invite people
+              </p>
+            </div>
+            <div className="video">
+              <video playsInline muted ref={myVideo} autoPlay />
+              <p className="you">You</p>
+              <p className="message">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque.
+              </p>
+              <div className="participants">
+                <div className="participant">
+                  <img
+                    src="./img/image.jpg"
+                    alt="image"
+                    className="participant__image"
+                  />
+                  <span className="icon">
+                    <MdMicOff />
+                  </span>
+                </div>
+
+                <div className="participant participant--speak">
+                  <img
+                    src="./img/image.jpg"
+                    alt="image"
+                    className="participant__image"
+                  />
+                  <span className="icon">
+                    <BsSoundwave />
+                  </span>
+                </div>
+                <div className="participant">
+                  <img
+                    src="./img/image.jpg"
+                    alt="image"
+                    className="participant__image"
+                  />
+                  <span className="icon">
+                    <MdMicOff />
+                  </span>
+                </div>
+                <div className="participant">
+                  <img
+                    src="./img/image.jpg"
+                    alt="image"
+                    className="participant__image"
+                  />
+                  <span className="icon">
+                    <MdMicOff />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </main>
+          <footer className="footer">
+            <div className="volume">
+              <span className="icon">
+                <MdVolumeUp />
+                <input type="range" name="" id="" className="slider" />
+              </span>
+              volume
+            </div>
+            <div className="options">
+              <div>
+                <span className="icon">
+                  <MdMicOff />
+                </span>
+                Mic
+              </div>
+              <div>
+                <span className="icon">
+                  <MdOutlineVideocamOff />
+                </span>
+                Cam
+              </div>
+              <div>
+                <span className="icon">
+                  <VscScreenFull />
+                </span>
+                Display
+              </div>
+              <div>
+                <span className="icon selected">
+                  <VscScreenFull />
+                </span>
+                Credit
+              </div>
+              <div>
+                <span className="icon">
+                  <MdOutlineFullscreen />
+                </span>
+                Screen
+              </div>
+            </div>
+            <div className="leave">
+              <span className="icon">
+                <IoLogOut />
+              </span>
+              leave
+            </div>
+          </footer>
         </div>
         <aside className="aside">
           <div className="cta">
@@ -58,7 +173,37 @@ const Callvideo = () => {
             <button className="btn">Participants (5) </button>
           </div>
           <div className="stat">
-            <h4 className="sat__title">Class X president election</h4>
+            <h6 className="stat__title">Class X president election</h6>
+            <div className="stat__items">
+              <div className="stat__item">
+                <div className="stat__progress">
+                  <div
+                    className="stat__progressbar"
+                    style={{
+                      width: "55%",
+                    }}
+                  ></div>
+                </div>
+                <div className="stat__info">
+                  <p className="stat__name">John Doe</p>
+                  <p className="stat__percent">55%</p>
+                </div>
+              </div>
+              <div className="stat__item">
+                <div className="stat__progress">
+                  <div
+                    className="stat__progressbar"
+                    style={{
+                      width: "25%",
+                    }}
+                  ></div>
+                </div>
+                <div className="stat__info">
+                  <p className="stat__name">John Doe</p>
+                  <p className="stat__percent">25%</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="chat">
             <div className="chat__header">
